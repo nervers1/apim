45055005fb737371966fa964863db4b107e5d543
kkimtae <kkimtae@gmail.com>
2021-02-26 10:42:02 +0900

    authorization exception 추가

---
+++ src/main/java/kr/mydata/apim/base/exception/AuthorizationException.java
@@ -0,0 +1,9 @@
+package kr.mydata.apim.base.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+public class AuthorizationException
+	extends Exception {
+}
