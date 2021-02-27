5877460a561805c58fd8720f97baafaf9c235a50
kkimtae <kkimtae@gmail.com>
2021-02-28 01:22:57 +0900

    은행업권 최신 API 규격 적용중...

--- src/main/java/kr/mydata/apim/vo/bank/ReqBank007.java
+++ src/main/java/kr/mydata/apim/vo/bank/ReqBank007.java
@@ -26,18 +26,21 @@ public class ReqBank007 {
     @Size(min = 1, max = 20, message = "account_num 값은 1 ~ 20 자리 입니다.")
     private String account_num;
 
-    @NotNull(message = "from_dtime 값이 반드시 있어야 합니다.")
-    @Size(min = 14, max = 14, message = "from_dtime 값은 14 자리 입니다.")
-    private String from_dtime;
+    @Size(min = 1, max = 7, message = "seqno 값은 1 ~ 7 자리 입니다.")
+    private String seqno;
 
-    @NotNull(message = "to_dtime 값이 반드시 있어야 합니다.")
-    @Size(min = 14, max = 14, message = "to_dtime 값은 14 자리 입니다.")
-    private String to_dtime;
+    @NotNull(message = "from_date 값이 반드시 있어야 합니다.")
+    @Size(min = 8, max = 8, message = "from_date 값은 8 자리 입니다.")
+    private String from_date;
+
+    @NotNull(message = "to_date 값이 반드시 있어야 합니다.")
+    @Size(min = 8, max = 8, message = "to_date 값은 8 자리 입니다.")
+    private String to_date;
 
     @Size(min = 1, max = 1000, message = "next_page 값은 1 ~ 1000 자리 입니다.")
     private String next_page;
 
-    @NotNull(message = "limit is null")
+    @NotNull(message = "limit 값이 반드시 있어야 합니다.")
     @Size(min = 1, max = 3, message = "limit 값은 1 ~ 3 자리 입니다.")
     private String limit;
 }
