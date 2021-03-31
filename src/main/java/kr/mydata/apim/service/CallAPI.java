5860c0d26f5adbce035e99b3705d3248043d3825
kkimtae <kkimtae@gmail.com>
2021-03-31 13:49:54 +0900

    API 호출 예제 commit

--- src/main/java/kr/mydata/apim/service/CallAPI.java
+++ src/main/java/kr/mydata/apim/service/CallAPI.java
@@ -13,113 +13,173 @@ import java.util.Map;
 
 public class CallAPI {
 
-	public String callGetAPI() {
+    public String callGetAPI() {
 
-		HashMap<String, Object> result = new HashMap<String, Object>();
+        HashMap<String, Object> result = new HashMap<String, Object>();
 
-		String resultBody = "";
+        String resultBody = "";
 
-		try {
-			// 연결설정
-			HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
-			factory.setConnectTimeout(3000);
-			factory.setReadTimeout(3000);
+        try {
+            // 연결설정
+            HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+            factory.setConnectTimeout(3000);
+            factory.setReadTimeout(3000);
 
-			RestTemplate restTemplate = new RestTemplate(factory);
+            RestTemplate restTemplate = new RestTemplate(factory);
 
-			// http header 설정
-			HttpHeaders header = new HttpHeaders();
-			header.add("Authorization", "Bearer [접근토큰]"); // 인증API로 생성한 접근토큰 설정
-			header.add("X-FSI-SVC-DATA-KEY", "N"); // N : 금보원제공 샘플데이터 이용, Y : 업로드데이터 이용
+            // http header 설정
+            HttpHeaders header = new HttpHeaders();
+            header.add("Authorization", "Bearer [접근토큰]"); // 인증API로 생성한 접근토큰 설정
+            header.add("X-FSI-SVC-DATA-KEY", "N"); // N : 금보원제공 샘플데이터 이용, Y : 업로드데이터 이용
 
-			// request parameter 설정
-			String org_code = "0000000";
-			String search_timestamp = "0";
-			String limit = "20";
+            // request parameter 설정
+            String org_code = "0000000";
+            String search_timestamp = "0";
+            String limit = "20";
 
-			StringBuilder parameter = new StringBuilder();
-			parameter.append("?" + "org_code=" + org_code);
-			parameter.append("&" + "search_timestamp=" + search_timestamp);
-			parameter.append("&" + "limit=" + limit);
+            StringBuilder parameter = new StringBuilder();
+            parameter.append("?" + "org_code=" + org_code);
+            parameter.append("&" + "search_timestamp=" + search_timestamp);
+            parameter.append("&" + "limit=" + limit);
 
-			HttpEntity<?> entity = new HttpEntity<>(header);
+            HttpEntity<?> entity = new HttpEntity<>(header);
 
-			String url = "http://localhost:7080/bank/accounts"; // 호출 API의 URL
-			UriComponents uri = UriComponentsBuilder.fromHttpUrl(url + parameter).build();
+            String url = "http://localhost:7080/bank/accounts"; // 호출 API의 URL
+            UriComponents uri = UriComponentsBuilder.fromHttpUrl(url + parameter).build();
 
-			ResponseEntity<Map> resultMap = restTemplate.exchange(uri.toString(), HttpMethod.GET, entity, Map.class);
-			result.put("statusCode", resultMap.getStatusCodeValue());
-			result.put("header", resultMap.getHeaders());
-			result.put("body", resultMap.getBody());
+            ResponseEntity<Map> resultMap = restTemplate.exchange(uri.toString(), HttpMethod.GET, entity, Map.class);
+            result.put("statusCode", resultMap.getStatusCodeValue());
+            result.put("header", resultMap.getHeaders());
+            result.put("body", resultMap.getBody());
 
-			ObjectMapper mapper = new ObjectMapper();
-			resultBody = mapper.writeValueAsString(resultMap.getBody());
+            ObjectMapper mapper = new ObjectMapper();
+            resultBody = mapper.writeValueAsString(resultMap.getBody());
 
-			System.out.println("result: " + resultMap);
-			System.out.println("result body : " + resultBody);
+            System.out.println("result: " + resultMap);
+            System.out.println("result body : " + resultBody);
 
-		} catch (Exception e) {
-			System.out.println(e.getLocalizedMessage());
-		}
+        } catch (Exception e) {
+            System.out.println(e.getLocalizedMessage());
+        }
 
-		return resultBody;
-	}
+        return resultBody;
+    }
 
-	public String callPostAPI() {
+    public String callPostAPI() {
 
-		HashMap<String, Object> result = new HashMap<String, Object>();
+        HashMap<String, Object> result = new HashMap<String, Object>();
 
-		String resultBody = "";
+        String resultBody = "";
 
-		try {
-			// 연결설정
-			HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
-			factory.setConnectTimeout(3000);
-			factory.setReadTimeout(3000);
+        try {
+            // 연결설정
+            HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+            factory.setConnectTimeout(3000);
+            factory.setReadTimeout(3000);
 
-			RestTemplate restTemplate = new RestTemplate(factory);
+            RestTemplate restTemplate = new RestTemplate(factory);
 
-			// http header 설정
-			HttpHeaders header = new HttpHeaders();
-			header.add("Authorization", "Bearer [접근토큰]"); // 인증API로 생성한 접근토큰 설정
-			header.add("X-FSI-SVC-DATA-KEY", "N"); // N : 금보원제공 샘플데이터 이용, Y : 업로드데이터 이용
-			header.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
+            // http header 설정
+            HttpHeaders header = new HttpHeaders();
+            header.add("Authorization", "Bearer [접근토큰]"); // 인증API로 생성한 접근토큰 설정
+            header.add("X-FSI-SVC-DATA-KEY", "N"); // N : 금보원제공 샘플데이터 이용, Y : 업로드데이터 이용
+            header.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
 
-			// request parameter 설정
-			String org_code = "0000000";
+            // request parameter 설정
+            String mydata_org_code = "";
+            String type = "";
+            String client_id = "";
+            String inquiry_date = "";
+            String stat_date_cnt = "";
+            Map<String, Object> stat_date_list = new HashMap<>();
 
-			Map<String, Object> params = new HashMap<>();
-			params.put("org_code", "0000000");
-			params.put("account_num", "09830487129182");
-			params.put("seqno", "000");
-			params.put("currency_code", "KRW");
-			params.put("search_timestamp", "0");
+            String stat_date = "";
+            String org_cnt = "";
+            Map<String, Object> org_list = new HashMap<>();
 
-			ObjectMapper objectMapper = new ObjectMapper();
-			String body = objectMapper.writeValueAsString(params);
+            String org_code = "";
+            String token_new = "";
+            String token_renew = "";
+            String token_revoke = "";
+            String token_own = "";
+            String api_type_cnt = "";
+            Map<String, Object> api_type_list = new HashMap<>();
 
-			HttpEntity<?> entity = new HttpEntity<>(body, header);
+            String api_type = "";
+            String tm_slot_cnt = "";
+            Map<String, Object> tm_slot_list = new HashMap<>();
 
-			String url = "http://localhost:7080/bank/accounts/deposit/basic"; // 호출 API의 URL
-			UriComponents uri = UriComponentsBuilder.fromHttpUrl(url).build();
+            String tm_slot = "";
+            String rsp_med = "";
+            String rsp_avg = "";
+            String rsp_total = "";
+            String rsp_stdev = "";
+            String status_cnt = "";
+            Map<String, Object> status_list = new HashMap<>();
 
-			ResponseEntity<Map> resultMap = restTemplate.exchange(uri.toString(), HttpMethod.POST, entity, Map.class);
-			result.put("statusCode", resultMap.getStatusCodeValue());
-			result.put("header", resultMap.getHeaders());
-			result.put("body", resultMap.getBody());
+            String status_code = "";
+            String api_call_cnt = "";
 
-			ObjectMapper mapper = new ObjectMapper();
-			resultBody = mapper.writeValueAsString(resultMap.getBody());
+            Map<String, Object> params = new HashMap<>();
+            params.put("mydata_org_code", mydata_org_code);
+            params.put("type", type);
+            params.put("client_id", client_id);
+            params.put("inquiry_date", inquiry_date);
+            params.put("stat_date_cnt", stat_date_cnt);
+            params.put("stat_date_list", stat_date_list);
 
-			System.out.println("result: " + resultMap);
-			System.out.println("result body : " + resultBody);
+            stat_date_list.put("stat_date", stat_date);
+            stat_date_list.put("org_cnt", org_cnt);
+            stat_date_list.put("org_list", org_list);
 
-		} catch (Exception e) {
-			System.out.println(e.getLocalizedMessage());
-		}
+            org_list.put("org_code", org_code);
+            org_list.put("token_new", token_new);
+            org_list.put("token_renew", token_renew);
+            org_list.put("token_revoke", token_revoke);
+            org_list.put("token_own", token_own);
+            org_list.put("api_type_cnt", api_type_cnt);
+            org_list.put("api_type_list", api_type_list);
 
-		return resultBody;
-	}
+            api_type_list.put("api_type", api_type);
+            api_type_list.put("tm_slot_cnt", tm_slot_cnt);
+            api_type_list.put("tm_slot_list", tm_slot_list);
+
+            tm_slot_list.put("tm_slot", tm_slot);
+            tm_slot_list.put("rsp_med", rsp_med);
+            tm_slot_list.put("rsp_avg", rsp_avg);
+            tm_slot_list.put("rsp_total", rsp_total);
+            tm_slot_list.put("rsp_stdev", rsp_stdev);
+            tm_slot_list.put("status_cnt", status_cnt);
+            tm_slot_list.put("status_list", status_list);
+
+            status_list.put("status_code", status_code);
+            status_list.put("api_call_cnt", api_call_cnt);
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            String body = objectMapper.writeValueAsString(params);
+
+            HttpEntity<?> entity = new HttpEntity<>(body, header);
+
+            String url = "http://localhost:7080/v1/mgmts/statistics"; // 호출 API의 URL
+            UriComponents uri = UriComponentsBuilder.fromHttpUrl(url).build();
+
+            ResponseEntity<Map> resultMap = restTemplate.exchange(uri.toString(), HttpMethod.POST, entity, Map.class);
+            result.put("statusCode", resultMap.getStatusCodeValue());
+            result.put("header", resultMap.getHeaders());
+            result.put("body", resultMap.getBody());
+
+            ObjectMapper mapper = new ObjectMapper();
+            resultBody = mapper.writeValueAsString(resultMap.getBody());
+
+            System.out.println("result: " + resultMap);
+            System.out.println("result body : " + resultBody);
+
+        } catch (Exception e) {
+            System.out.println(e.getLocalizedMessage());
+        }
+
+        return resultBody;
+    }
 }
 
 
